f961cb47d1b23b055c2049272cdaa7b2770ae693
amirulkamil <amirul.kamil@gmail.com>
2018-01-07 22:22:28 +0800

    all tabbed layout

--- app/src/main/java/co/xtvt/xtvt/ActivityDashboard.java
+++ app/src/main/java/co/xtvt/xtvt/ActivityDashboard.java
@@ -1,8 +1,8 @@
 package co.xtvt.xtvt;
 
-import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Handler;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.FragmentManager;
 import android.content.Intent;
@@ -12,6 +12,7 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog;
@@ -20,6 +21,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TabHost;
 import android.widget.Toast;
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -86,16 +88,25 @@ public class ActivityDashboard extends AppCompatActivity implements NavigationVi
             FragmentManager fragmentManager = getSupportFragmentManager();
             FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
+            TabLayout tabLayout = findViewById(R.id.tabs);
+            ViewPager viewPager = findViewById(R.id.container);
+
             switch (item.getItemId()) {
                 case R.id.navigation_home:
+                    tabLayout.setVisibility(View.GONE);
+                    viewPager.setVisibility(View.GONE);
                     getSupportActionBar().setTitle(R.string.title_home);
                     fragmentTransaction.replace(R.id.frameBottomNav,new FragmentBottomNavHome()).commit();
                     return true;
                 case R.id.navigation_explore:
+                    tabLayout.setVisibility(View.VISIBLE);
+                    viewPager.setVisibility(View.VISIBLE);
                     getSupportActionBar().setTitle(R.string.title_explore);
                     fragmentTransaction.replace(R.id.frameBottomNav,new FragmentBottomNavExplore()).commit();
                     return true;
                 case R.id.navigation_notifications:
+                    tabLayout.setVisibility(View.GONE);
+                    viewPager.setVisibility(View.GONE);
                     getSupportActionBar().setTitle(R.string.title_notifications);
                     fragmentTransaction.replace(R.id.frameBottomNav,new FragmentBottomNavNotifications()).commit();
                     return true;
@@ -131,6 +142,7 @@ public class ActivityDashboard extends AppCompatActivity implements NavigationVi
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.toolbar_main, menu);
+        menu.findItem(R.id.action_settings).setVisible(false);
         return true;
     }
 
@@ -161,6 +173,10 @@ public class ActivityDashboard extends AppCompatActivity implements NavigationVi
             startActivity(intent);
         } else if (id == R.id.nav_drawer_activities) {
             Intent intent = new Intent(ActivityDashboard.this, ActivityActivities.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+        } else if (id == R.id.nav_drawer_groups) {
+            Intent intent = new Intent(ActivityDashboard.this, ActivityGroups.class);
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
             startActivity(intent);
         } else if (id == R.id.nav_drawer_logout) {
